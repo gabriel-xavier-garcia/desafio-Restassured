fc69207ee8b17034744592db2bc57c3eaba98556
Gabriel Garcia <gabrielproaj@gmail.com>
2026-02-10 20:51:33 -0300

    Find All 2, FindById Completed

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -2,9 +2,13 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.tests.TokenUtil;
 import org.json.JSONException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -13,12 +17,11 @@ import static org.hamcrest.Matchers.*;
 @Transactional
 public class MovieControllerRA {
 
-
-
 	private Long existingMovieId, nonExistingMovieId;
 	private String adminUsername, adminPassword, clientUsername, clientPassword;
 	private String adminToken, clientToken, invalidToken;
-	private String empty;
+
+	private Map<String, Object> postMovieInstance;
 
 	@BeforeEach
 	public void setUp(){
@@ -30,10 +33,15 @@ public class MovieControllerRA {
 		adminPassword = "123456";
 		clientUsername = "maria@gmail.com";
 		clientPassword = "123456";
-		empty = "";
 
 		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
 		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+
+		postMovieInstance = new HashMap<>();
+		postMovieInstance.put("title", "Test Movie");
+		postMovieInstance.put("score", 0.0);
+		postMovieInstance.put("count", 0);
+		postMovieInstance.put("image", "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
 
 	}
 	
@@ -52,19 +60,45 @@ public class MovieControllerRA {
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+				.when()
+				.get("/movies?title=Venom")
+				.then()
+				.statusCode(200)
+				.body("content[0].id", is(2))
+				.body("content[0].title", equalTo("Venom: Tempo de Carnificina"))
+				.body("content[0].score",is(3.3F))
+				.body("content[0].count", is(3))
+				.body("content[0].image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		given()
+				.when()
+				.get("/movies/{id}", existingMovieId)
+				.then()
+				.statusCode(200)
+				.body("id", is(1))
+				.body("title", equalTo("The Witcher"))
+				.body("score", is(4.5F))
+				.body("count", is(2))
+				.body("image",equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+		given()
+				.when()
+				.get("/movies/{id}", nonExistingMovieId)
+				.then()
+				.statusCode(404);
 	}
 	
 	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() {
+
 	}
 	
 	@Test
