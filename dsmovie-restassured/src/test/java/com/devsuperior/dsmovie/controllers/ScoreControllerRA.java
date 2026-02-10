0da490d6e86213ec17f8d1690cd27821bb5d175e
Gabriel Garcia <gabrielproaj@gmail.com>
2026-02-10 15:51:04 -0300

    first commit

---
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dsmovie.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class ScoreControllerRA {
+	
+	@Test
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+	}
+	
+	@Test
+	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+	}
+	
+	@Test
+	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
+	}
+}
